b63ab0cbb8b068552a0e603d73832b378bbae1a4
mikelino77 <angel67mo@gmail.com>
2026-03-03 21:46:45 -0600

    Mike front_1 03/03 9:40

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// Tus importaciones (Admin)
+import { Catalogo } from './admin/catalogo/catalogo';
+import { AgregarProducto } from './admin/agregar-producto/agregar-producto';
+import { TableroPedidos } from './admin/tablero-pedidos/tablero-pedidos';
+
+// Las importaciones de Mike (Cliente y Auth)
+import { Login } from './auth/login/login';
+import { Buscador } from './cliente/buscador/buscador';
+import { DetalleProducto } from './cliente/detalle-producto/detalle-producto';
+import { Carrito } from './cliente/carrito/carrito';
+import { Checkout } from './cliente/checkout/checkout';
+
+export const routes: Routes = [
+  // --- TUS RUTAS (ADMIN) ---
+  { path: 'admin/catalogo', component: Catalogo },
+  { path: 'admin/agregar-producto', component: AgregarProducto },
+  { path: 'admin/tablero-pedidos', component: TableroPedidos },
+
+  // --- LAS RUTAS DE MIKE (CLIENTE) ---
+  { path: 'login', component: Login },
+  { path: 'tienda', component: Buscador },
+  { path: 'producto', component: DetalleProducto },
+  { path: 'carrito', component: Carrito },
+  { path: 'checkout', component: Checkout }
+];
---
+++ frontend/src/app/cliente/buscador/buscador.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-buscador',
+  imports: [],
+  templateUrl: './buscador.html',
+  styleUrl: './buscador.css',
+})
+export class Buscador {
+
+}
